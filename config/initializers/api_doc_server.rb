d1644a9fc44894c11a104f0f3750fddcc505f910
Benjamin Fleischer <github@benjaminfleischer.com>
2017-10-11 12:11:37 -0500

    Add sendgrid

--- config/initializers/api_doc_server.rb
+++ config/initializers/api_doc_server.rb
@@ -2,5 +2,6 @@ ApiDocServer.configure do |config|
   config.swagger_urls = {
     local_petstore: 'api/petstore/swagger.yaml',
     remote_petstore: 'http://petstore.swagger.io/v2/swagger.json',
+    sendgrid: 'https://api.stoplight.io/v1/versions/E2KbzZkqZQttfyqxp/export/oas.json', # https://sendgrid.com/docs/API_Reference/api_v3.html
   }
 end
